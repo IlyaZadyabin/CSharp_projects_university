b11220b50c1d87d07e23a5d065d0e7b8c5274c16
IlyaZadyabin <z.ilya.007@gmail.com>
2020-11-17 22:07:03 +0300

    Lab2 final version

---
+++ Lab2/Lab2/Grid.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab2
+{
+    public struct Grid {
+        public Grid(float t0_, float time_step_, int amount_of_nodes_) {
+            t0 = t0_;
+            time_step = time_step_;
+            amount_of_nodes = amount_of_nodes_;
+        }
+
+        public override string ToString() {
+            return t0.ToString() + " " + time_step.ToString() + " " + amount_of_nodes.ToString();
+        }
+        public float t0 { get; set; }
+        public float time_step { get; set; }
+        public int amount_of_nodes { get; set; }
+        public string ToString(string format) {
+            return t0.ToString(format) + " " + time_step.ToString(format) + " " + amount_of_nodes.ToString(format);
+        }
+    }
+}
---
+++ Lab2/Lab2/V1DataCollection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Numerics;
+
+namespace Lab2
+{
+    public class V1DataCollection : V1Data, IEnumerable<DataItem> {
+        public V1DataCollection(string str_, DateTime date_) : base(str_, date_) {}
+
+        // info
+        // date
+        // time x,y,z
+        // time x,y,z
+        // ...
+        public V1DataCollection(string filename) : base("", new DateTime()) {
+            try {
+                string[] lines = System.IO.File.ReadAllLines(filename);
+                info = lines[0];
+                date = Convert.ToDateTime(lines[1]);
+
+                for (int i = 2; i < lines.Length; i++) {
+                    string[] tokens = lines[i].Split(' ');
+                    string[] coord = tokens[1].Split(',');
+
+                    list.Add(new DataItem
+                    {
+                        t = Convert.ToSingle(tokens[0]),
+                        vec = new Vector3(Convert.ToSingle(coord[0]),
+                                           Convert.ToSingle(coord[1]),
+                                           Convert.ToSingle(coord[2]))
+                    });
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void InitRandom(int nItems, float tmin, float tmax, float minValue, float maxValue) {
+            Random random = new System.Random();
+            for (int i = 0; i < nItems; i++) {
+                DataItem tmp = new DataItem();
+                tmp.t = tmin + (float)random.NextDouble() * (tmax - tmin);
+                tmp.vec = new Vector3(
+                                        minValue + (float)random.NextDouble() * (maxValue - minValue),
+                                        minValue + (float)random.NextDouble() * (maxValue - minValue),
+                                        minValue + (float)random.NextDouble() * (maxValue - minValue)
+                                    );
+                list.Add(tmp);
+            }
+        }
+        public override float[] NearZero(float eps) {
+            float[] nodes = new float[list.Count];
+            int length = 0;
+            for (int i = 0; i < list.Count; i++) {
+                float tmp = list[i].vec.Length();
+                if (tmp < eps) {
+                    nodes[length++] = list[i].t;
+                }
+            }
+            float[] ans = new float[length];
+
+            Array.Copy(nodes, ans, length);
+            return ans;
+        }
+
+        public override string ToString() {
+            return "V1DataCollection" + " " + base.ToString() + " " + list.Count.ToString();
+        }
+        public override string ToLongString() {
+            string str = ToString() + Environment.NewLine;
+            for (int i = 0; i < list.Count; i++) {
+                str += list[i].ToString() + Environment.NewLine;
+            }
+            return str;
+        }
+        public override string ToLongString(string format) {
+            string str = "V1DataCollection" + " " + base.ToLongString(format)
+                    + " " + list.Count.ToString(format) + Environment.NewLine;
+            for (int i = 0; i < list.Count; i++) {
+                str += list[i].ToString(format) + Environment.NewLine;
+            }
+            return str;
+        }
+        public IEnumerator<DataItem> GetEnumerator() {
+            return list.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+        public List<DataItem> list { get; set; } = new List<DataItem>();
+    }
+}
