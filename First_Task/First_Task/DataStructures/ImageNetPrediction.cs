cb0539ddc8ea28ce8becde6f633c52e01106b81e
IlyaZadyabin <z.ilya.007@gmail.com>
2021-10-03 22:11:57 +0300

    First_Task works without parallel

---
+++ First_Task/First_Task/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace First_Task.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
